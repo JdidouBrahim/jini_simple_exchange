796d8d0d6e51eec8acadcc2b47704465a9373a3c
Brahim JDIDOU <brahimjdidou20@gmail.com>
2018-04-08 19:45:50 +0000

    jini - simple exchange

---
+++ src/HelloIntf.java
@@ -0,0 +1,7 @@
+import java.rmi.RemoteException;
+
+public interface HelloIntf {
+
+    public String getHello() throws RemoteException;
+    public void setHello(String message) throws RemoteException;
+}
